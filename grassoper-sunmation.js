2fa512d90554539a4c81fefe45f556ce34a60cf1
ozibozkurt <ozipaki@gmail.com>
2022-12-15 20:48:37 +0000

    grassoper sunmation

--- grassoper-sunmation.js
+++ grassoper-sunmation.js
@@ -8,3 +8,10 @@ For example (Input -> Output):
 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
 */
+var summation = function (num) {
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
