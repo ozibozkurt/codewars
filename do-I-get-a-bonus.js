eeb8efa5b0b3f1a872bd837200166a9b4c65af01
ozibozkurt <ozipaki@gmail.com>
2022-12-16 12:21:59 +0000

    do I get a bonus

--- do-I-get-a-bonus.js
+++ do-I-get-a-bonus.js
@@ -8,3 +8,10 @@ If bonus is true, the salary should be multiplied by 10. If bonus is false, the 
 Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java, Scala, and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell, and Lua) or "¥" (Rust).
 
 */
+function bonusTime(salary, bonus) {
+  if (bonus === true) {
+    return "£" + salary * 10;
+  } else {
+    return "£" + salary;
+  }
+}
