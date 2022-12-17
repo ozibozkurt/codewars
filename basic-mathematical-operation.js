9cabde71a9e24e302c2f39eb9fac1781c7195821
ozibozkurt <ozipaki@gmail.com>
2022-12-17 20:39:41 +0000

    basic mathematical operation

--- basic-mathematical-operation.js
+++ basic-mathematical-operation.js
@@ -12,3 +12,17 @@ Examples(Operator, value1, value2) --> output
 
 
 */
+function basicOp(operation, value1, value2) {
+  if (operation == "+") {
+    return value1 + value2;
+  }
+  if (operation == "-") {
+    return value1 - value2;
+  }
+  if (operation == "*") {
+    return value1 * value2;
+  }
+  if (operation == "/") {
+    return value1 / value2;
+  }
+}
