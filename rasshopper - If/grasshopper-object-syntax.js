1f468f2e5cfaa566775e7719baaa3be49cc88e55
ozibozkurt <ozipaki@gmail.com>
2022-12-19 22:07:53 +0000

    grasshopper syntax

--- rasshopper - If/grasshopper-object-syntax.js
+++ rasshopper - If/grasshopper-object-syntax.js
@@ -5,3 +5,19 @@ Unfortunately, the game is not working.
 Find all of the errors in the rooms object to get your game working again.
 
 */
+var rooms = {
+  first: {
+    description: "This is the first room",
+    items: {
+      chair: "The old chair looks comfortable",
+      lamp: "This lamp looks ancient",
+    },
+  },
+  second: {
+    description: "This is the second room",
+    items: {
+      couch: "This couch looks like it would hurt your back",
+      table: "On the table there is an unopened bottle of water",
+    },
+  },
+};
