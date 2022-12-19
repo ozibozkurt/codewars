466ce9316d91447b8b77a8f28f4de07490c6898d
ozibozkurt <ozipaki@gmail.com>
2022-12-19 22:12:46 +0000

    even or odd

--- rasshopper - If/evenorodd.js
+++ rasshopper - If/evenorodd.js
@@ -6,3 +6,7 @@ and returns "Even" for even numbers or "Odd" for odd numbers.
 function even_or_odd(number) {
   return Math.abs(number) % 2 == 1 ? "Odd" : "Even";
 }
+
+function even_or_odd(number) {
+  return Math.abs(number) % 2 == 1 ? "Odd" : "Even";
+}
