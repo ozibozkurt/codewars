e74a83207692a147ed849201930154394e9ae71c
ozibozkurt <ozipaki@gmail.com>
2022-12-19 22:12:23 +0000

    even or odd

--- rasshopper - If/evenorodd.js
+++ rasshopper - If/evenorodd.js
@@ -3,3 +3,6 @@ Create a function that takes an integer as an argument
 and returns "Even" for even numbers or "Odd" for odd numbers.
 
 */
+function even_or_odd(number) {
+  return Math.abs(number) % 2 == 1 ? "Odd" : "Even";
+}
